adc70e9babb5f0d6828b87773288a768e866e716
hychem95 <sunghwan.martin.kim@gmail.com>
2020-12-10 23:12:46 -0500

    Add files via upload
    Uploading a file ...... by SK

---
+++ exercises.py
@@ -0,0 +1,26 @@
+# Exercise 1:
+# Write a function that takes the base and height of a triangle and returns the
+# area.
+
+# Exercise 2:
+# Write a function that takes a list of animals and prints them each out in all
+# capital letters.
+
+# Exercise 3:
+# Write a function that takes a string as an argument and returns the number of
+# vowels in that string.
+
+# Exercise 4:
+# Write a function that takes a string of words and returns them capitalized as
+# if they were a title.
+# e.g.: "united states of america" --> "United States Of America"
+
+# Exercise 5:
+# Write a function that takes a string of words and returns the words reversed,
+# but the words are still in the same order.
+# e.g.: "United States of America" --> "detinU setatS fo aciremA"
+
+# Exercise 6:
+# Write a function that takes in a number, and returns a list of all of its
+# divisors.
+# e.g.: 12 --> [1, 2, 3, 4, 6, 12]
